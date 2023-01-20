c9d176a5c8a9e6c03891367cb79aa3b0b89c404e
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2023-01-20 17:51:45 +0200

    added task1

---
+++ LAB1/Task1/js/js.js
@@ -0,0 +1,33 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+// slider
+// displaying the first slide
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+// n is 1 or -1 indicating for next and prev
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
